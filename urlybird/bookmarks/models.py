92f79b7aa02cbd9ca26bfa2ec2a6359fef5ac5e4
Jermaine Kee <godson919@icloud.com>
2015-10-15 14:40:29 -0400

    starting short model

--- urlybird/bookmarks/models.py
+++ urlybird/bookmarks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Short(models.Model):
+    user = models.ForeignKey(User)
+    # bookmark
+    # short_url
+    # timestamp
+    # title
+    # description
