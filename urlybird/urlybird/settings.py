37c89b19424fc836de85dff98abfeaa76c4bbbdd
Jermaine Kee <godson919@icloud.com>
2015-10-16 12:52:36 -0400

    initial database setup

--- urlybird/urlybird/settings.py
+++ urlybird/urlybird/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'urlybird.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bookmarks',
+        'USER': 'bookmarks',
+        'PASSWORD': 'bookmarks',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
