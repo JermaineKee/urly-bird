50ecbf4ff902747d67e73edefb7d61e41391970e
Glenn Hurley <glenn.hurley@gmail.com>
2015-10-15 20:34:19 -0400

    Really basic redirect on the homepage of the app to google.com.

--- urlybird/urlybird/urls.py
+++ urlybird/urlybird/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from bookmarks.views import redirect_url
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', redirect_url)
 ]
